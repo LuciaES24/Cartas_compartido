5e775929314a5e4e284d8f96a1f317e0c1f844a4
AdrianPRG <alopgal962@g.educaand.es>
2023-02-15 01:20:49 +0100

    Creacion de Clase Baraja Y Clase Mano

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,18 +6,3 @@ fun main(args: Array<String>) {
     println("Program arguments: ${args.joinToString()}")
 }
 
-class Baraja {
-    var palos_posibles = listOf("c", "r", "p", "t")
-    var numeros_posibles = listOf(1,2,3,4,5,6,7,8,9,10,11,12,13)
-}
-
-class Mano (size : Int) {
-    var maximoCartas = 3
-    var listaCartas = mutableListOf<Carta>()
-
-    constructor():this(3){
-
-    }
-
-
-}
---
+++ src/main/kotlin/Mano.kt
@@ -0,0 +1,10 @@
+class Mano (size : Int) {
+    var maximoCartas = 3
+    var listaCartas = mutableListOf<Carta>()
+
+    constructor():this(3){
+
+    }
+
+
+}
